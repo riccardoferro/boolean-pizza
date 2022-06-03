6d03dcf69f83b363c7b3c064266d20e31265524c
riccardoferro <riccardo.ferron8@gmail.com>
2022-06-03 16:35:59 +0200

    init project laravel 7
    insert composer
    
    insert npm
    
    did initials settings
    
    did initial emigrate

---
+++ app/Pizza.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pizza extends Model
+{
+    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pizze';
+    protected $fillable = ['nome_pizza'];
+
+}
